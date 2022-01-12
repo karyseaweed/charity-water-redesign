a43ed50a69aa12ac862a8fd9caf40ebfff196b3c
karyseaweed <karyhdev@gmail.com>
2022-01-11 23:41:09 -0500

    CHORE: update gitignore

--- .next/static/mLWxVAY7DSYAOoJ9ZtVqA/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/959-5311424f4187cdaf.js","static/chunks/pages/index-a2b2f2632c5df194.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
